7fe74aecd44c265fb28c9c7ebc01a1d811fd3123
Trung Dang <2017.12ch.34.dangductrung@gmail.com>
2020-07-08 23:20:35 +0700

    first blood

---
+++ app/src/main/java/com/adida/dailycook/MainActivity.java
@@ -0,0 +1,14 @@
+package com.adida.dailycook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
