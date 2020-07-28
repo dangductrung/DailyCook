39ecef01d57f3fcfadf805d2e7ad8e2bd75dd84d
phithang711 <pthang769@gmail.com>
2020-07-28 10:31:12 +0700

    draft

---
+++ app/src/main/java/com/adida/dailycook/login/LoginPage.java
@@ -0,0 +1,16 @@
+package com.adida.dailycook.login;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.adida.dailycook.R;
+
+public class LoginPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activivity_login);
+    }
+}
