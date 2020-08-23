e1ad2feac40c8717e6677ab9468d5660f020db20
Trung Dang <2017.12ch.34.dangductrung@gmail.com>
2020-08-24 00:35:14 +0700

    login ui

--- app/src/main/java/com/adida/dailycook/login/LoginPage.java
+++ app/src/main/java/com/adida/dailycook/login/LoginPage.java
@@ -1,18 +1,58 @@
 package com.adida.dailycook.login;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.adida.dailycook.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginPage extends AppCompatActivity {
+    TextInputEditText email, pass;
+    Button login;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-
         setContentView(R.layout.activivity_login);
+
+        email = findViewById(R.id.edit_email);
+        pass = findViewById(R.id.edit_password);
+        login = findViewById(R.id.btn_login);
+        setupEditTextListen();
+    }
+
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    void setupEditTextListen() {
+        email.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        });
+
+        pass.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        });
     }
 }
